cffabacf17bf9e6f37b9dd3c1312305c82346ae2
Ariel Sidi <ariel.sidi@blackwoodseven.com>
2019-01-31 12:34:21 +0100

    Eslint + Prettier + Flow set up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+// @flow
 import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => {
-  return <div>Hello React!</div>;
+    return <div>Hello React!</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
+    template: "./src/index.html",
+    filename: "./index.html"
 });
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
-  plugins: [htmlPlugin]
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
 };
